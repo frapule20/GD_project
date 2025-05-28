25014bdf277a1329fd8daac958fa90253dd01c16
Francesca Pulerà <f.pulera@campus.unimib.it>
2025-05-28 18:49:47 +0200

    Sistemato lo script dell'arma

--- Assets/Shooting.cs
+++ Assets/Shooting.cs
@@ -1,16 +1,26 @@
 using UnityEngine;
 
 public class Shooting : MonoBehaviour
+
+
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    public GameObject bullet; // Proiettile da istanziare
 
     // Update is called once per frame
     void Update()
     {
+        // 1. Controllare se vine cliccato il mouse
+        if (Input.GetButtonDown("Fire1"))
+        {
+            // 2. Se sì, creare un proiettile
+            GameObject bulletInstance =  Instantiate(
+                bullet, transform.position, transform.rotation);
+
+            // 3. Assegnare la direzione del proiettile e una forza
+            bulletInstance.GetComponent<Rigidbody>().AddForce(
+                transform.forward * 1000f, ForceMode.Impulse);
+        }
+
         
     }
 }
