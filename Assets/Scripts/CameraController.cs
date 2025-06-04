e4773a28acc1f2de79a7c06b91a59b6eff1d717f
AnnaMarika01 <a.biasco@campus.unimib.it>
2025-06-04 16:06:11 +0200

    Aggiunta TopDown Mode per mod Hide in Camera

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -17,11 +17,21 @@ public class CameraController : MonoBehaviour
     [SerializeField] bool invertX;
     [SerializeField] bool invertY;
 
+
+    [Header("Top-Down Mode")]
+    [SerializeField] Vector3 topDownOffset = new Vector3(0, 5, 0);
+    [SerializeField] float transitionSpeed = 3f;
+
     float rotationX;
     float rotationY;
 
     float invertXVal;
     float invertYVal;
+
+    
+    private Vector3 normalPosition;
+    private Quaternion normalRotation;
+    private bool wasInTopDown = false;
 
     private void Start()
     {
@@ -30,6 +40,17 @@ public class CameraController : MonoBehaviour
     }
 
     private void Update()
+    {
+        if (PlayerController.IsHidden)
+        {
+            TopDownMode();
+        }
+        else
+        {
+            NormalMode();
+        }
+    }
+   private void NormalMode()
     {
         invertXVal = (invertX) ? -1 : 1;
         invertYVal = (invertY) ? -1 : 1;
@@ -42,9 +63,43 @@ public class CameraController : MonoBehaviour
         var targetRotation = Quaternion.Euler(rotationX, rotationY, 0);
         var focusPosition = followTarget.position + new Vector3(framingOffset.x, framingOffset.y);
 
-        transform.position = focusPosition - targetRotation * new Vector3(0, 0, distance);
-        transform.rotation = targetRotation;
+        Vector3 targetPosition = focusPosition - targetRotation * new Vector3(0, 0, distance);
 
+        if (wasInTopDown)
+        {
+            transform.position = Vector3.Lerp(transform.position, targetPosition, Time.deltaTime * transitionSpeed);
+            transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, Time.deltaTime * transitionSpeed);
+            
+            if (Vector3.Distance(transform.position, targetPosition) < 0.1f && 
+                Quaternion.Angle(transform.rotation, targetRotation) < 1f)
+            {
+                wasInTopDown = false;
+            }
+        }
+        else
+        {
+            transform.position = targetPosition;
+            transform.rotation = targetRotation;
+        }
+    }
+
+    private void TopDownMode()
+    {
+        if (!wasInTopDown)
+        {
+            wasInTopDown = true;
+        }
+
+        Vector3 targetPosition = followTarget.position + topDownOffset;
+        Quaternion targetRotation = Quaternion.LookRotation(Vector3.down);
+
+        transform.position = Vector3.Lerp(transform.position, targetPosition, Time.deltaTime * transitionSpeed);
+        transform.rotation = Quaternion.Lerp(transform.rotation, targetRotation, Time.deltaTime * transitionSpeed);
+    }
+
+    public bool IsInTopDownMode()
+    {
+        return PlayerController.IsHidden;
     }
     public Quaternion PlanarRotation => Quaternion.Euler(0, rotationY, 0);
 }
